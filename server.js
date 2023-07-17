f881e957f5e520cbdde1acd22dac6dd006e1e69d
Florian <florian@ROGER.localdomain>
2023-07-17 12:26:03 +0200

    add route test

--- server.js
+++ server.js
@@ -36,10 +36,11 @@ app.get('*', (req, res) => res.status(501).send('What the hell are you doing !?!
 /********************************/
 /*** Start serveur avec test DB */
 
-app.listen(process.env.SERVER_PORT, () => {
+const blairot = app.listen(process.env.SERVER_PORT, () => {
   console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
 })
 
+module.exports = blairot
 
 
 
---
+++ test/route.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest')
+
+let server
+
+describe('Router TEST', () => {
+
+    beforeEach(() => {
+        server = require('../server')
+    })
+    afterEach(() => {
+        server.close()
+    })
+
+
+    describe('GET ON /', () => {
+        it('Should return 200 status', async () => {
+            const response = await request(server).get('/')
+            expect(response.status).toBe(200)
+        })
+
+        it('Should return 501 status', async () => {
+            const response = await request(server).get('/fngjqkdfvgk')
+            expect(response.status).toBe(501)
+        })
+    })
+
+    describe('TEST ON MARCEL', () => {
+        it('Should return 200 status', async () => {
+            const response = await request(server).get('/marcel/12')
+            expect(response.status).toBe(200)
+        })
+        it('Should return 204 status', async () => {
+            const response = await request(server).delete('/marcel/5')
+            expect(response.status).toBe(204)
+        })
+    })
+})
