dab5b82d3a05f9f602b865d921e110d2df24db02
Florian <florian@ROGER.localdomain>
2023-07-17 13:42:56 +0200

    fix route marcel

--- routers/marcel.js
+++ routers/marcel.js
@@ -27,6 +27,6 @@ router.put('', marcelCtrl.addMarcel)
 
 router.patch('/:id', marcelCtrl.updateMarcel)
     
-router.delete('/idiot/:id', marcelCtrl.deleteMarcel)
+router.delete('/:id', marcelCtrl.deleteMarcel)
 
 module.exports = router
